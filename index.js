b234eed709adf8a9331847cc5906719837aa4cc6
wangyanyan.warryy <wangyanyan.warryy@bytedance.com>
2023-01-31 21:47:48 +0800

    feat(quickSort): 修正

--- index.js
+++ index.js
@@ -4,41 +4,52 @@ function swapArr(arr, i, j) {
   arr[j] = temp;
 }
 
-function radixSort(arr) {
-  /** 是否已经排序到数据最高位数 */
-  let flag = false;
-  let base = 10;
+function quickSort(arr, left, right) {
   const len = arr.length;
-  /**
-   * 这里如果用.fill(new Array()), 则下面进行 bucket[radix].push(n) 的时候, 一次会把所有bucket中的数组全部push进去 n
-   */
-  const bucket = new Array(10);
+  // 通过 left, right 的数组下标, 来实现数组的切分, 用于递归
+  if (left === undefined || right === undefined) {
+    left = 0;
+    right = len - 1;
+  }
 
-  while (!flag) {
-    flag = true;
-    for (let i = 0; i < len; i++) {
-      const r = arr[i] / base;
-      if (r >= 10) {
-        flag = false;
-        base *= 10;
-      }
-      const floorR = Math.floor(r);
-      const diff = r - floorR;
-      const radix = Math.floor(diff * 10);
-      bucket[radix] ? bucket[radix].push(arr[i]) : bucket[radix] = [arr[i]];
-    }
+  let i = left,
+    j = right;
+  let temp;
+  const n = arr[i];
 
-    let j = 0;
-    for (let i = 0; i < 10; i++) {
-      while (bucket[i].length) {
-        arr[j++] = bucket[i].shift();
+  while (i < j) {
+    if (arr[i] > arr[j]) {
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      if (arr[j] == n) {
+        i++;
+      } else {
+        j--;
+      }
+    } else {
+      if (arr[i] === n) {
+        j--;
+      } else {
+        i++;
       }
     }
+  }
+  console.log([...arr])
+  if (i > left) {
+    quickSort(arr, left, i - 1)
+  }
+
+  if (i < right) {
+    quickSort(arr, i + 1, right)
   }
 }
 
 var arr = [4, 5, 6, 7, 3, 5, 2, 1, 8, 9, 0];
+// var arr = [3, 2, 5, 1, 4];
+
+console.log(arr);
 console.time();
-const res = radixSort(arr);
+quickSort(arr);
 console.timeEnd();
 console.log(arr);
--- sort/quick-sort/README.md
+++ sort/quick-sort/README.md
@@ -16,38 +16,43 @@
 1. 递归实现
 ```javascript
 function quickSort(arr, left, right) {
-  const len = arr.length
+  const len = arr.length;
   // 通过 left, right 的数组下标, 来实现数组的切分, 用于递归
   if (left === undefined || right === undefined) {
-    left = 0
-    right = len - 1
+    left = 0;
+    right = len - 1;
   }
 
-  let i = left, j = right
-  let temp
-  const n = arr[i]
+  let i = left,
+    j = right;
+  let temp;
+  const n = arr[i];
 
   while (i < j) {
     if (arr[i] > arr[j]) {
-      temp = arr[i]
-      arr[i] = arr[j]
-      arr[j] = temp
-      if (arr[i] == n) {
-        i++
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      if (arr[j] == n) {
+        i++;
       } else {
-        j--
+        j--;
       }
     } else {
-      j--
+      if (arr[i] === n) {
+        j--;
+      } else {
+        i++;
+      }
     }
   }
-
-  if (i > 0) {
-    quickSort(arr, 0, i - 1)
+  console.log([...arr])
+  if (i > left) {
+    quickSort(arr, left, i - 1)
   }
 
-  if (j < right) {
-    quickSort(arr, j + 1, right)
+  if (i < right) {
+    quickSort(arr, i + 1, right)
   }
 }
 ```
